334d1c8dedf24be0958adc3bda6598416f6ee210
Adrien Gerard <agerard@altissia.org>
2022-08-06 23:00:11 +0200

    feat(labo-0001): shuffle visual reels

--- src/reel/reel.service.ts
+++ src/reel/reel.service.ts
@@ -2,7 +2,7 @@ import { VisualReel } from './Reel';
 
 export class ReelService {
     getVisualReelByReelId(): VisualReel {
-        return [
+        return shuffle([
             { picture: '&#127815;', name: 'blueberry' },
             { picture: '&#127818;', name: 'orange' },
             { picture: '&#127819;', name: 'lemon' },
@@ -14,6 +14,26 @@ export class ReelService {
             { picture: '&#128276;', name: 'bell' },
             { picture: '&#128292;', name: 'bar' },
             { picture: '&#128640;', name: 'rocket' },
+        ])
+    }
+}
+
+export function shuffle<T>(array: T[]): T[] {
+    let currentIndex = array.length,
+        randomIndex
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--
+
+        // And swap it with the current element.
+        ;[array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
         ]
     }
+
+    return array
 }
